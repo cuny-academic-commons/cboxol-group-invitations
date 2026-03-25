687cc3b1e364d99ef4129b7dd8e32d60cd147d78
Boone B Gorges <boonebgorges@gmail.com>
2026-03-25 13:56:31 -0500

    Fixes for phpstan.

--- templates/groups/single/nav/membership.php
+++ templates/groups/single/nav/membership.php
@@ -8,7 +8,8 @@
  * @package CBOX\OL\GroupInvitations
  */
 
-$group = groups_get_current_group();
+$group        = groups_get_current_group();
+$group_status = ( $group instanceof \BP_Groups_Group ) ? $group->status : null;
 
 $current_tab = bp_action_variable( 0 );
 
@@ -17,7 +18,7 @@ $current_tab = bp_action_variable( 0 );
 <?php if ( bp_is_item_admin() ) : ?>
 	<li class="<?php echo 'manage-members' === $current_tab ? 'current-menu-item' : ''; ?>"><a href="<?php echo esc_attr( bp_get_group_manage_url( $group, bp_groups_get_path_chunks( [ 'manage-members' ], 'manage' ) ) ); ?>"><?php esc_html_e( 'Membership', 'cboxol-group-invitations' ); ?></a></li>
 
-	<?php if ( 'private' === $group->status ) : ?>
+	<?php if ( 'private' === $group_status ) : ?>
 		<li class="<?php echo 'membership-requests' === $current_tab ? 'current-menu-item' : ''; ?>"><a href="<?php echo esc_attr( bp_get_group_manage_url( $group, bp_groups_get_path_chunks( [ 'membership-requests' ], 'manage' ) ) ); ?>"><?php esc_html_e( 'Member Requests', 'cboxol-group-invitations' ); ?></a></li>
 	<?php endif; ?>
 <?php else : ?>
